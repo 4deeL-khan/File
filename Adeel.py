e6c21e7b6719222ea92eaf9afd27105c0a96a3e4
4deeL-khan <110104681+4deeL-khan@users.noreply.github.com>
2023-04-14 14:13:30 +0500

    Update Adeel.py
    pkg update
    pkg upgrade
    pkg install python
    pkg install python
    pip install requests
    pip install mechanize
    pip install lolcat
    pip install bs4
    pkg install git
    git clone https://GitHub.com/4deel-khan/File.git
    cd File
    python2 Adeel.py

--- Adeel.py
+++ Adeel.py
@@ -1,28 +1,86 @@
-import os, time, requests, datetime, random, multiprocessing.pool, getpass, json, threading, sys, uuid, shutil, zlib, base64
-from multiprocessing.pool import ThreadPool
-from requests.exceptions import ConnectionError
-os.system('rm -rf .txt')
-for n in range(5000):
-    nmbr = random.randint(1111111, 9999999)
-    sys.stdout = open('.txt', 'a')
-    print nmbr
-    sys.stdout.flush()
-
-l1 = '100078'
-l2 = '100077'
-os.system('rm -rf token.txt')
-g = '\x1b[1;92m'
-r = '\x1b[1;91m'
-w = '\x1b[1;97m'
-y = '\x1b[1;93m'
-n = '\x1b[1;94m'
-gu = '\x1b[1;95m'
-sm = '\x1b[1;96m'
+import requests,bs4,json,os,sys,random,datetime,time,re
 try:
-    import lolcat
-except:
-    os.system('pip2 install lolcat')
+	import rich
+except ImportError:
+	os.system('pip install rich')
+	time.sleep(1)
+	try:
+		import rich
+	except ImportError:
+		exit('[✓] Internet Eror ,Install Manual (pip install rich)')
+from rich.table import Table as me
+from rich.console import Console as sol
+from bs4 import BeautifulSoup as parser
+from concurrent.futures import ThreadPoolExecutor as tred
+from rich.console import Group as gp
+from rich.panel import Panel as nel
+from rich import print as cetak
+from rich.markdown import Markdown as mark
+from rich.columns import Columns as col
+try:ugen = open('user.txt','r').read().splitlines()
+except:ugen = ['Mozilla/4.1 (compatible; MSIE 5.0; Symbian OS; Nokia 7610;451) Opera 6.20'] #Aziz
+try:ugen2 = open('user2.txt','r').read().splitlines()
+except:ugen2 = ['Mozilla/4.1 (compatible; MSIE 5.0; Symbian OS; Nokia 7610;451) Opera 6.20'] #Aziz
 
+id,id2,loop,ok,cp,akun,oprek,method,lisensiku,taplikasi,tokenku,uid,lisensikuni= [],[],0,0,0,[],[],[],[],[],[],[],[]
+
+x = '\033[93m'
+k = '\033[93m'
+h = '\x1b[1;92m'
+hh = '\033[92m'
+u = '\033[95m'
+kk = '\033[93m'
+b = '\33[1;96m'
+p = '\x1b[1;95m'
+P = '\033[0;00m'
+J = '\033[1;93m'
+S = '\033[0;00m'
+N = '\x1b[0m'
+I ='\033[1;92m'
+C ='\033[1;96m'
+M ='\033[1;91m'
+U ='\033[1;95m'
+K ='\033[1;93m'
+P='\033[00m'
+h='\033[1;90m'
+Q="\033[00m"
+kk='\033[1;92m'
+ff='\033[1;96m'
+G='\033[1;96m'
+p='\033[00m'
+h='\033[1;90m'
+Q="\033[00m"
+I='\033[1;92m'
+II='\033[1;96m'
+m='\033[1;91m'
+O ='\033[1;93m'
+H='\033[1;93m'
+b = '\033[1;96m'
+war = "[•]"
+B = random.choice([U,I,K,b,M])
+
+dic = {'1':'Januari','2':'Februari','3':'Maret','4':'April','5':'Mei','6':'Juni','7':'Juli','8':'Agustus','9':'September','10':'Oktober','11':'November','12':'Desember'}
+dic2 = {'01':'Januari','02':'Februari','03':'Maret','04':'April','05':'Mei','06':'Juni','07':'Juli','08':'Agustus','09':'September','10':'Oktober','11':'November','12':'Desember'}
+tgl = datetime.datetime.now().day
+bln = dic[(str(datetime.datetime.now().month))]
+thn = datetime.datetime.now().year
+okc = 'OK-'+str(tgl)+'-'+str(bln)+'-'+str(thn)+'.txt'
+cpc = 'CP-'+str(tgl)+'-'+str(bln)+'-'+str(thn)+'.txt'
+
+my_id = '100007061760822'
+
+def jalan(z):
+    for e in z + "\n":sys.stdout.write(e);sys.stdout.flush();time.sleep(0.04)
+def mlaku(z):
+    for e in z + "\n":sys.stdout.write(e);sys.stdout.flush();time.sleep(0.03)
+    
+def clear():
+	os.system('clear')
+def back():
+	menu()
+def banner():
+	clear()
+	print("""%s\n\x1b[93;1m
     logo ="""
  \033[1;94m _______      \033[1;93m _     _     \033[1;95m _______
     \033[1;94m |         \033[1;93m |_____|     \033[1;95m |______
